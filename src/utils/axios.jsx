cbb8116b529dfd2e297ed573680dcc39aba34f6f
Olayinka O <omosehintemilade@gmail.com>
2022-08-22 10:20:29 +0100

    Testing API baseurl

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log({ API_BASE_URL });
 // Axios configurations
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
